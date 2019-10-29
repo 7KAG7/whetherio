5c3356dadf785c7b30f80266763d28020a89a6d6
7KAG7 <guernsey.keith@gmail.com>
2019-10-29 16:40:48 -0400

    added images and containers

--- app/javascript/react/components/FootwearContainer.js
+++ app/javascript/react/components/FootwearContainer.js
@@ -1,17 +1,22 @@
 import React from "react"
 
 const FootwearContainer = props => {
+
   return (
     <div className="body-boxes grid-x medium-6">
+
       <h6 htmlFor="head-box-1-label" className="box-label ">Footwear Accessory 1:
       <box type="text" className="head-box"></box>
       </h6>
+
       <h6 htmlFor="head-box-2-label" className="box-label">Footwear Accessory 2:
       <box type="text" className="head-box"></box>
       </h6>
+
       <h6 htmlFor="head-box-3-label" className="box-label">Footwear Accessory 3:
       <box type="text" className="head-box"></box>
       </h6>
+
     </div>
   )
 }
