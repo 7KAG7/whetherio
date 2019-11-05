41703ffdc74378cf84c16f4c245368de5fe27a0d
7KAG7 <guernsey.keith@gmail.com>
2019-11-05 09:57:25 -0500

    index page css and clothing tiles

---
+++ app/javascript/react/components/FootwearTile.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect }  from "react"
+
+
+const FootwearTile = props => {
+
+  return (
+    <div>
+      <div>FootwearTile</div>
+    </div>
+  )
+}
+
+export default FootwearTile
---
+++ app/javascript/react/components/HeadTile.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect }  from "react"
+
+
+const HeadTile = props => {
+
+  return (
+    <div>
+      <div>head tile</div>
+    </div>
+  )
+}
+
+export default HeadTile
