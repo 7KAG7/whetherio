8e1c95cd0bb0ce7959b74a44ddc8c81eeafd653e
7KAG7 <guernsey.keith@gmail.com>
2019-11-16 07:54:14 -0500

    remove blank spaces throughout

--- app/javascript/react/components/BodyBoxContainer.js
+++ app/javascript/react/components/BodyBoxContainer.js
@@ -7,33 +7,6 @@ import LegsTile from "./LegsTile"
 import FootwearTile from "./FootwearTile"
 
 const BodyBoxContainer = props => {
-<<<<<<< HEAD
-=======
-  const [currentBodyWeather, setCurrentBodyWeather] = useState ([])
-
-  useEffect(() => {
-    fetch('api/v1/weathers')
-    .then(response => {
-      return response.json()
-    })
-    .then(fetchedBodyWeather => {
-      setCurrentBodyWeather(fetchedBodyWeather.hourly.data)
-    })
-  }, [])
-
-  const hourly = currentBodyWeather.map((hour) => {
-    return(
-      <HeadTile
-        key={hour.id}
-        time={hour.time}
-        currently={hour.currently}
-        temperature={hour.temperature}
-        summary={hour.summary}
-      />
-    )
-  })
-
->>>>>>> c46a46be1f05063ca75771a1ebae83dfd041ccb2
 
   return (
     <div>
--- app/javascript/react/components/FootwearTile.js
+++ app/javascript/react/components/FootwearTile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect }  from "react"
 
-
 const FootwearTile = props => {
 
   return (
