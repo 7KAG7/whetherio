b95a52b08df8f8fbbb7cc034ab85877bd09b2460
7KAG7 <guernsey.keith@gmail.com>
2019-12-05 13:45:29 -0500

    corrected footwear tile

--- app/javascript/react/components/FootwearTile.js
+++ app/javascript/react/components/FootwearTile.js
@@ -17,227 +17,227 @@ const FootwearTile = props => {
     })
   }, [])
 
-  let legs
+  let footwear
     if (currentBodyWeather.temperature > 80 && currentBodyWeather.summary.contains("Rain")) {
-      (legs =  <div>
+      (footwear =  <div>
 
                 </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 80 && currentBodyWeather.summary.contains("Drizzle")) {
-      (legs =  <div>
+      (footwear =  <div>
 
                 </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 80 && currentBodyWeather.summary.contains("Clear")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 80 && currentBodyWeather.summary.contains("Cloudy")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 80 && currentBodyWeather.summary.contains("Overcast")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 60 && currentBodyWeather.temperature < 79 && currentBodyWeather.summary.includes("Clear")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 60 && currentBodyWeather.temperature < 79 && currentBodyWeather.summary === ("Cloudy")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 60 && currentBodyWeather.temperature < 79 && currentBodyWeather.summary === ("Overcast")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 60 && currentBodyWeather.temperature < 79 && currentBodyWeather.summary === ("Rain")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 60 && currentBodyWeather.temperature < 79 && currentBodyWeather.summary === ("Drizzle")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 30 && currentBodyWeather.temperature < 59  && currentBodyWeather.summary.includes("Overcast")) {
-      (legs = <div>
+      (footwear = <div>
                 <a href="https://www.amazon.com/Carhartt-Washed-Twill-Dungaree-Relaxed/dp/B00870NC9G/ref=as_li_ss_il?dchild=1&keywords=mens+pants&psc=1&qid=1572979819&s=apparel&sr=1-45&linkCode=li1&tag=whetherio-20&linkId=fcd3d79801060578f30bc3c6466cde00&language=en_US" target="_blank">
                 <img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00870NC9G&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=whetherio-20&language=en_US" />
                 </a>
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 30 && currentBodyWeather.temperature < 59 && currentBodyWeather.summary === ("Rain")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 30 && currentBodyWeather.temperature < 59 && currentBodyWeather.summary === ("Drizzle")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 30 && currentBodyWeather.temperature < 59 && currentBodyWeather.summary.includes("Clear")) {
-      (legs = <div>
+      (footwear = <div>
                 <a target="_blank"  href="https://www.amazon.com/gp/product/B005FPU1VS/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B005FPU1VS&linkCode=as2&tag=whetherio-20&linkId=e2ae29d5578dc91c0fc7f957bea60c14">
                 <img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=B005FPU1VS&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL110_&tag=whetherio-20" />
                 </a>
               </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 30 && currentBodyWeather.temperature < 59 && currentBodyWeather.summary.includes("Cloudy")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature > 30 && currentBodyWeather.temperature < 59 && currentBodyWeather.summary.includes("Overcast")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature < 29 && currentBodyWeather.summary.includes("Clear")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature < 29 && currentBodyWeather.summary.includes("Cloudy")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature < 29 && currentBodyWeather.summary.includes("Overcast")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
       }
     } else if (currentBodyWeather.temperature < 29 && currentBodyWeather.summary.includes("Snow")) {
-      (legs = <div>
+      (footwear = <div>
 
                </div>)
       if (user) {
         if (user === "female") {
-          (legs = <div>
+          (footwear = <div>
 
                    </div>)
         }
@@ -246,9 +246,9 @@ const FootwearTile = props => {
 
   return (
     <div>
-      {legs}
+      {footwear}
     </div>
   )
 }
-  
+
 export default FootwearTile
